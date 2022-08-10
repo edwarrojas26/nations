0e44f2fac32ea09b02e8ee70731075ddfd62267f
unknown <jhonalexcamargo07@gmail.com>
2022-08-10 13:29:57 -0500

    relaciones con modelos

--- app/Models/Continent.php
+++ app/Models/Continent.php
@@ -5,14 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Continent extends Model
+class continent extends Model
 {
     //la tabla a conectar
     protected $table = "continents";
-    // clave primaria de la tabla
+    //la clave primaria de la tabla
     protected $primaryKey = "continent_id";
-    //omitir campos de auditoria
-    public $timestamps = false;
+    //omitir campos de uditoria
+    public $tiempstamps = false;
     use HasFactory;
-
+    
+    //relacion entre continente y sus regiones
+    public function regiones(){
+        //parameters
+        //1. linked model
+        //2. foreign key of current model into Related model Region
+        return $this->hasMany(Region::class, 'continent_id');
+    }
+    //relacion entre continete y paises
+    // Continen: Granfather
+    //Region: Papá
+    //country: nieto 
+    public function paises(){
+        // parametros
+        // 1- nieto
+        // 2- papa
+        // 3- Fk del abuelo en el papa
+        // 4- clave foranea del papa en el nieto
+        return $this->hasManyThrough(Country::class,
+                            Region::class, 
+                            'continent_id',
+                            'region_id');
+    }
+    
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,10 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
+    
+    //la tabla a conectar
     protected $table = "countries";
-    // clave primaria de la tabla
-    protected $primaryKey = "countries_id";
-    //omitir campos de auditoria
-    public $timestamps = false;
+    //la clave primaria de la tabla
+    protected $primaryKey = "country_id";
+    //omitir campos de uditoria
+    public $tiempstamps = false;
     use HasFactory;
+    use HasFactory;
+
+    //Many to Manyy country - language relationship
+    public function languages(){
+        //belongstomany method
+        //1. related model
+        //2. pivot table(intermediate table)
+        //3. foreign key of current model
+        //4. foreign key ofrelated model
+        return $this->belongsToMany(Language::class, 'country_languages',
+                                                     'country_id',
+                                                     'language_id' );
+    }
+
+    //M:1 country - region
+    //relationship
+    public function regions(){
+        //BelongsTo to method: parameters
+        //1. related model
+        //2. foreign key of related model in current model
+        return $this->belongsTo(Region::class , 'region_id');
+    }
+
+    
 }
